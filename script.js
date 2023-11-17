b20f3f97546f22a04b6dd6f0520b2b2a4ff4f300
MarceloCris <149599863+MarceloCris@users.noreply.github.com>
2023-11-17 11:17:42 -0300

    inserindo o atributo para troca de imagens no botão

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 
 function toggleMode() {
   const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img(queryselector busca o seletor no documento, no caso usar a mesma tag CSS, pois foi a mesma que usou para modificar)
+  const img = document.querySelector("#profile img")
+
+  //substituir a img e se tiver no light mode ja adiocionar com o setAttribute e o SRC(onde se encontra)
   if (html.classList.contains("light")) {
-    html.classList.remove("light")
+    img.setAttribute("src", "./assets/marcelo-light.png")
   } else {
-    html.classList.add("light")
+    img.setAttribute("src", "./assets/marcelo-dark.png")
   }
 }
+
+//se tiver sem light mode, manter a img normal
