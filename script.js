c2c78075cb98009244cce6b783059fa6376a290a
MarceloCris <149599863+MarceloCris@users.noreply.github.com>
2023-11-17 10:10:41 -0300

    funcção toggle

---
+++ script.js
@@ -0,0 +1,15 @@
+/* DOCUMENT - representa o documento que estamos trabalhando. o (.) faz com que acesse minha tag HTML
+html - Onde na estrutura do doc / DocumentoElement - - mostra qual elemento dentro do HTML iremos movivmentar.*/
+
+function toggleMode() {
+  alert('hello man')
+}
+  /*const html = document.documentElement
+
+  if (html.classlist.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+}
+//html.classlist.toggle('light')
